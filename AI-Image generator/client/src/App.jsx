d40b659f9b454052fb53c0b863ed0e33b23009fd
PrathamK78 <pratham1054.be21@chitkara.edu.in>
2024-03-19 19:10:41 +0530

    Add first file

---
+++ AI-Image generator/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter, Link, Route,Routes} from 'react-router-dom';
+import {logo} from './assets';
+
+
+const App = () => {
+  return (
+    <h1 className='text-3x1 font-bold underline'>Hello World!!</h1>
+  )
+}
+
+export default App
